9f96c22516108650e7796d188e206d1759bdf7bf
nicbudd <nicbudd@nicbudd.com>
2023-01-29 00:44:33 -0500

    Running DreamCheated on GPU (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-//#![feature(portable_simd)]
 //use std::env;
+use arrayfire::*;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
@@ -7,13 +7,12 @@ use std::time::SystemTime;
 use rand::prelude::*;
 use rand_xoshiro::Xoshiro256PlusPlus;
 use chrono::{DateTime, Utc};
-use rayon::prelude::*;
 
-fn update(max_rods: u32, max_pearls: u32, max_attempts: u64, when_found: &str, attempts: u64,
-    total_exec_time: f64, speed: f64) {
+fn update(maxRods: u16, maxPearls: u16, maxAttempts: u64, whenFound: &str, attempts: u64,
+    totalExecTime: f64, speed: f64) {
 
 
-    let writestring = format!("{},{},{},{},{},{},{}", max_rods, max_pearls, max_attempts, when_found, attempts, total_exec_time, speed);
+    let writestring = format!("{},{},{},{},{},{},{}", maxRods, maxPearls, maxAttempts, whenFound, attempts, totalExecTime, speed);
 
     let mut file = File::create("dream.txt")
         .expect("Could not open the dream.txt file to write.");
@@ -22,7 +21,7 @@ fn update(max_rods: u32, max_pearls: u32, max_attempts: u64, when_found: &str, a
     file.write(writestring.as_bytes())
         .expect("Could not write to the dream.txt file.");
 
-    println!("Attempts:{}, Rods: {}, Pearls: {}, Speed: {} attempts/sec", attempts, max_rods, max_pearls, speed);
+    println!("Attempts:{}, Rods: {}, Pearls: {}, Speed: {} attempts/sec", attempts, maxRods, maxPearls, speed);
 
 }
 
@@ -30,20 +29,26 @@ fn main() {
 
     const PEARLTHRESHOLD: f64 = 20.0;
     const PEARLRANGE: f64 = 423.0;
-    const GOLDTRADES: u32 = 262;
-    const DREAMPEARLS: u32 = 42;
-    const DREAMRODS: u32 = 211;
+    const GOLDTRADES: u16 = 262;
+    const DREAMPEARLS: u16 = 42;
 
-    let mut max_rods;
-    let mut max_pearls;
-    let mut max_attempts;
+    const BLAZETHRESHOLD: f64 = 1.0;
+    const BLAZERANGE: f64 = 2.0;
+    const BLAZEKILLS: u16 = 305;
+    const DREAMRODS: u16 = 211;
 
-    let mut attempts;
-    let mut when_found;
-    let mut total_exec_time;
-    let mut speed;
+    const LOGFREQ: u64 = 1_000_000; // After how many attempts does it update the file
+    //const BATCH_SIZE: u64 = 1<<20;
+    const BATCH_SIZE: u64 = 16;
 
-    let mut batch_size: u64 = 100_000;
+    let mut maxRods: u16 = 0;
+    let mut maxPearls: u16 = 0;
+    let mut maxAttempts: u64 = 0;
+
+    let mut attempts: u64 = 0;
+    let mut whenFound = String::new();
+    let mut totalExecTime: f64 = 0.0;
+    let mut speed: f64 = 0.0;
 
 
     // load previous data from file
@@ -52,90 +57,94 @@ fn main() {
 
     let split_string: Vec<&str> = contents.split(",").collect();
 
-    max_rods = split_string[0].parse::<u32>().unwrap();
-    max_pearls = split_string[1].parse::<u32>().unwrap();
-    max_attempts = split_string[2].parse::<u64>().unwrap();
-    when_found = String::from(split_string[3]);
+    maxRods = split_string[0].parse::<u16>().unwrap();
+    maxPearls = split_string[1].parse::<u16>().unwrap();
+    maxAttempts = split_string[2].parse::<u64>().unwrap();
+    whenFound = String::from(split_string[3]);
     attempts = split_string[4].parse::<u64>().unwrap();
-    total_exec_time = split_string[5].parse::<f64>().unwrap();
+    totalExecTime = split_string[5].parse::<f64>().unwrap();
+    speed = split_string[6].parse::<f64>().unwrap();
+
+    let dims = Dim4::new(&[BATCH_SIZE, 1, 1, 1]);
+
+    let m1 = constant(0x5555555555555555u64, dims);
+    let m2 = constant(0x3333333333333333u64, dims);
+    let m4 = constant(0x0f0f0f0f0f0f0f0fu64, dims);
+    let h01 = constant(0x0101010101010101u64, dims);
+
 
     // main loop
     loop {
+        let mut rng = Xoshiro256PlusPlus::from_entropy();
         //let mut frng = fastrand::seed(rng.gen());
 
         // start timer
         let now = SystemTime::now();
 
-        if attempts >= 500_000_000 {
-            batch_size = 500_000_000;
-        } else if attempts >= 100_000_000 {
-            batch_size = 100_000_000;
-        } else if attempts >= 1_000_000 {
-            batch_size = 1_000_000;
-        }
-
-        
-        let rods: Vec<u32> = (0..batch_size).into_par_iter()
-                                .map_init(
-                                    || Xoshiro256PlusPlus::from_entropy(),
-                                    |rng, _x| (rng.gen::<u128>(), rng.gen::<u128>(), rng.gen::<u64>() & 0x1FFFF_FFFF_FFFFu64),
-                                )
-                                .map(|x| x.0.count_ones() + x.1.count_ones() + x.2.count_ones())
-                                .filter(|x| *x >= max_rods || *x >= DREAMRODS)
-                                .collect();
-
-        let mut pearls: Vec<(u32, u32)> = rods.clone().into_par_iter()
-                                        .map_init(
-                                            || Xoshiro256PlusPlus::from_entropy(),
-                                            |rng, x| (x, (0..GOLDTRADES).into_iter()
-                                                            .map(|_y| rng.gen_bool(PEARLTHRESHOLD / PEARLRANGE) as u32)
-                                                            .sum()),
-                                        )
-                                        .filter(|x| (x.1 >= max_pearls || x.1 >= DREAMPEARLS))
-                                        .collect();
 
 
-        let new_len = pearls.len();
-        
-        if new_len > 0 {
-            let new_record;
 
-            if new_len == 1 {
-                new_record = pearls.get(0).expect("Expected 1 item in list of length 1.");
-            } else {
-                println!("{} in batch higher than record.", new_len);
-                pearls.sort_by(|b, a| a.0.partial_cmp(&b.0).unwrap());
-                pearls.sort_by(|b, a| a.1.partial_cmp(&b.1).unwrap());
+        let mut rodArray = randu::<u64>(dims);
 
-                new_record = pearls.get(0).expect("Expected 1 item in list of length greater than 1.");
+        // popcount64c from https://en.wikipedia.org/wiki/Hamming_weight
+        rodArray = &rodArray - (&(&rodArray >> 1u64) & &m1);
+
+        let max = max_all(&rodArray);
+        println!("{:?}", &max);
+
+
+        for i in 0..LOGFREQ {
+
+            // RODS
+            let mut rodCount: u16 = 0;
+
+            // super duper efficient way
+            let list: [u64; 5] = [rng.gen(), rng.gen(), rng.gen(), rng.gen(), rng.gen::<u64>() & 0x1FFFF_FFFF_FFFFu64]; // 49 1s as a mask
+
+
+            //let mut counted: u16 = 0;
+            // why generate 305 random numbers when you can generate just 5?
+            for item in list {
+                rodCount += item.count_ones() as u16; // fast bit count go brrrrrrr
+                /*if (rodCount + (305u16 - counted) < maxRods) { // if we can tell right away we aren't gonna get a max
+                    continue;
+                }*/
+                //counted += 64;
             }
 
-            
 
-            let utc: DateTime<Utc> = Utc::now();
-            when_found = utc.format("%F %T").to_string();
-            max_attempts = attempts;
-            max_rods = new_record.0;
-            max_pearls = new_record.1;
+            // only do pearl section if rod section gets record (optimization)
+            if rodCount >= maxRods {
+                // PEARLS
+                let mut pearlCount: u16 = 0;
 
-            println!("New record: {} rods, {} pearls", max_rods, max_pearls);
-        }        
-        
-        attempts += batch_size;                               
-                                        
-        //dbg!(&rods);
-        //dbg!(pearls);
+                for j in 0..GOLDTRADES {
+                    pearlCount += rng.gen_bool(PEARLTHRESHOLD / PEARLRANGE) as u16;
+                }
 
-        //std::process::exit(0);
+                let newMax = (rodCount >= maxRods && pearlCount >= maxPearls) ||
+                (rodCount >= DREAMRODS && pearlCount >= maxPearls) ||
+                (rodCount >= maxRods && pearlCount >= DREAMPEARLS);
 
+                if newMax {
+                    let utc: DateTime<Utc> = Utc::now();
+                    whenFound = utc.format("%F %T").to_string();
+                    maxAttempts = attempts;
+                    maxRods = rodCount;
+                    maxPearls = pearlCount;
+                    update(maxRods, maxPearls, maxAttempts, &whenFound, attempts, totalExecTime, speed);
+                }
+            }
+            attempts += 1;
+        }
 
         // stop timer
         match now.elapsed() {
             Ok(elapsed) => {
                 let seconds = elapsed.as_secs_f64();
-                speed = batch_size as f64 / seconds;
-                total_exec_time += seconds;
-                update(max_rods, max_pearls, max_attempts, &when_found, attempts, total_exec_time, speed);
+                speed = LOGFREQ as f64 / seconds;
+                totalExecTime += seconds;
+                update(maxRods, maxPearls, maxAttempts, &whenFound, attempts, totalExecTime, speed);
             }
             Err(e) => {
                 println!("Error: {:?}", e);
